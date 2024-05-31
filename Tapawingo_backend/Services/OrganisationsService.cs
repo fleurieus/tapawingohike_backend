4bcc28ee8a03acc02f83218124ca6ecea1f81463
Twan_01 <513647@student.saxion.nl>
2024-05-31 11:39:27 +0200

    Created POST-Organisations endpoint

---
+++ Tapawingo_backend/Services/OrganisationsService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Tapawingo_backend.Dtos;
+using Tapawingo_backend.Interface;
+
+namespace Tapawingo_backend.Services
+{
+    public class OrganisationsService
+    {
+        private readonly IOrganisationsRepository _organisationsRepository;
+
+        public OrganisationsService(IOrganisationsRepository organisationsRepository)
+        {
+            _organisationsRepository = organisationsRepository;
+        }
+
+        public OrganisationDto CreateOrganisation(CreateOrganisationDto organisationName)
+        {
+            return _organisationsRepository.CreateOrganisation(organisationName);
+        }
+    }
+}
