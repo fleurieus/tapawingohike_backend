eb73e49c704bef15f857387ca785321bf289d6d7
Vincent Veluwenkamp <513500@student.saxion.nl>
2024-06-10 15:59:53 +0200

    Added FKs and cascade for Database

--- Tapawingo_backend/Models/Event.cs
+++ Tapawingo_backend/Models/Event.cs
@@ -3,8 +3,9 @@
     public class Event
     {
         public int Id { get; set; }
-        public string Name { get; set; }
         public int OrganisationId { get; set; }
+        public Organisation Organisation { get; set; }
+        public string Name { get; set; }
         public ICollection<UserEvent> Users { get; set; }
         public ICollection<Edition> Editions { get; set; }
     }
--- Tapawingo_backend/Models/TWRoute.cs
+++ Tapawingo_backend/Models/TWRoute.cs
@@ -3,9 +3,9 @@
     public class TWRoute
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Routepart> Routeparts { get; set; }
         public int EditionId { get; set; }
         public Edition Edition { get; set; }
+        public string Name { get; set; }
+        public ICollection<Routepart> Routeparts { get; set; }
     }
 }
