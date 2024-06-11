c76011fb8c8fb22ea5394afbb4450476df1efdd0
Vincent Veluwenkamp <513500@student.saxion.nl>
2024-06-11 11:14:54 +0200

    removed unnecessarycomments

--- Tapawingo_backend/Models/Team.cs
+++ Tapawingo_backend/Models/Team.cs
@@ -2,34 +2,18 @@
 
 namespace Tapawingo_backend.Models
 {
-    // Model class representing a team
     public class Team
     {
-        // Unique identifier for the team
         public int Id { get; set; }
         public int EditionId { get; set; }
         public Edition Edition { get; set; }
         public string Name { get; set; }
-
-        // Code associated with the team
         public string Code { get; set; }
-
-        // Name of the contact person for the team
         public string ContactName { get; set; }
-
-        // Email address of the contact person for the team
         public string ContactEmail { get; set; }
-
-        // Phone number of the contact person for the team
         public string ContactPhone { get; set; }
-
-        // Indicates whether the team operates online
         public bool Online { get; set; }
-
-        // Collection of location logs associated with the team
         public ICollection<Locationlog> Locationlogs { get; set; }
-
-        // Collection of route parts associated with the team
         public ICollection<TeamRoutepart> TeamRouteparts { get; set; }
     }
 }
